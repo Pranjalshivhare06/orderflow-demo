819f4bac0007f1eb68bb05a5165b8dc87297b32b
Pranjal Shivhare <pranjalshivhare1202@gmail.com>
2025-11-26 21:06:36 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 require('dotenv').config();
 const cors = require('cors'); // ← Add this line
 
